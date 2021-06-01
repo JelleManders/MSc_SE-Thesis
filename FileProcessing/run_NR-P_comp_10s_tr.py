b76cf3f6b15eb9c9aa3196549836bfccc3419783
JelleManders <jelle@shifter.nl>
2021-06-01 10:01:23 +0200

    Modified the different metric gatherers to write to their own results and log files, and analyse their appropriate section of videos

--- FileProcessing/run_NR-P_comp_10s_tr.py
+++ FileProcessing/run_NR-P_comp_10s_tr.py
@@ -7,23 +7,23 @@ from Blockiness import blockiness
 from Tools import LiveVD_dis_cat, t
 
 
-def run_NR_P_comp_10s_BlueSky():
-	log_file = open("log.txt", "w")
-	log_file.write(t() + " > comp_10s_bs: Starting NR-P metrics of reference videos...")
-	results_file = open("../resources/LiveVD/Scores/NR-P_comp_10s_bs_scores.txt", "w")
-	log_file.write(t() + " > comp_10s_bs: Opened results file...")
-	for video in LiveVD_dis_cat("bs"):
+def run_NR_P_comp_10s_Tractor():
+	log_file = open("Logs/NR-P_comp_10s_tr.txt", "w")
+	log_file.write(t() + " > Starting NR-P metrics of Tractor compressed videos...")
+	results_file = open("../resources/LiveVD/Scores/NR-P_comp_10s_tr_scores.txt", "w")
+	log_file.write(t() + " > Opened results file...")
+	for video in LiveVD_dis_cat("tr"):
 		try:
-			log_file.write(t() + " > comp_10s_bs: Starting analysis of " + video)
+			log_file.write(t() + " > Starting analysis of " + video)
 			blur = blur_laplacian(768, 432, video)
-			log_file.write(t() + " > comp_10s_bs: finished blur: " + str(blur))
+			log_file.write(t() + " > finished blur: " + str(blur))
 			noise_mean, noise_ratio = noise_mean_ratio(768, 432, video)
-			log_file.write(t() + " > comp_10s_bs: finished noise: mean = " + str(noise_mean) + ", ratio = " + str(noise_ratio))
+			log_file.write(t() + " > finished noise: mean = " + str(noise_mean) + ", ratio = " + str(noise_ratio))
 			block = blockiness(768,432, video)
-			log_file.write(t() + " > comp_10s_bs: finished blockiness: " + str(blockiness))
+			log_file.write(t() + " > finished blockiness: " + str(blockiness))
 			motion = motion_intensity(768, 432, video)
-			log_file.write(t() + " > comp_10s_bs: finished motion intensity: " + str(motion))
-			log_file.write(t() + " > comp_10s_bs: Finished analysis of " + video)
+			log_file.write(t() + " > finished motion intensity: " + str(motion))
+			log_file.write(t() + " > Finished analysis of " + video)
 
 			results_file.write(
 				video[37_39] + "\t" + video[-12:-4] + "\t" +
@@ -33,10 +33,10 @@ def run_NR_P_comp_10s_BlueSky():
 				"Block:   " + str(round(block, 5)) + "\t" + 
 				"Motion:  " + str(round(motion, 5)) + "\t"
 			)
-			log_file.write(t() + " > comp_10s_bs: Wrote results of " + video + " to results file.")
+			log_file.write(t() + " > Wrote results of " + video + " to results file.")
 		except Exception as e:
 			results_file.write(video[37_39] + "\t" + video[-12:-4] + "\t" + "Error occured: " + str(e))
-			log_file.write(t() + " > comp_10s_bs: Error occured on file " + video + ", error message: " + str(e))
-	log_file.write(t() + " > comp_10s_bs: Finished analysis of all videos! closing results_file...")
+			log_file.write(t() + " > Error occured on file " + video + ", error message: " + str(e))
+	log_file.write(t() + " > Finished analysis of all videos! closing results_file...")
 	results_file.close()
-	log_file.write(t() + " > comp_10s_bs: Done. Results can be found in ../resources/LiveVD/Scores/NR-P_ref_scores.txt")
+	log_file.write(t() + " > Done. Results can be found in ../resources/LiveVD/Scores/NR-P_comp_10s_tr_scores.txt")
