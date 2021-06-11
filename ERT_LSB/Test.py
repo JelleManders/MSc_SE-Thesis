123106f9ac4a0463a581f7c1a875fbde2311cee5
JelleManders <jelle@shifter.nl>
2021-06-11 10:37:04 +0200

    Inserted metric scores into datasets.py, now includes all the values from the 10s videos. Tweaked the model in test.py

--- ERT_LSB/Test.py
+++ ERT_LSB/Test.py
@@ -3,13 +3,13 @@
 # Learning Rate: 0.01
 
 # explore random forest number of trees effect on performance
-from numpy import mean
-from numpy import std
+from numpy import mean, std, corrcoef
 from sklearn.datasets import make_regression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import cross_val_predict
 from sklearn.ensemble import RandomForestRegressor
-# from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
 import DataSets as ds
 
@@ -30,16 +30,27 @@ def evaluate_model(model, X, y):
 	cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
 	# evaluate the model and collect the results
 	scores = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1, error_score='raise')
-	return scores
+	predictions = cross_val_predict(model, X, y)
+	return scores, predictions
 
 # define dataset
-X, y = ds.data_ref()
+Metric_data, VMAF_labels = ds.data_10s()
 
 # construct a ensemble regression tree
 ert = get_model()
 
 # evaluate the model
-scores = evaluate_model(ert, X, y)
+scores, predictions = evaluate_model(ert, Metric_data, VMAF_labels)
 
 # summarize the performance
 print('>%s %.3f (%.3f)' % ("test", mean(scores), std(scores)))
+
+# Calculate the Pearson Coefficient of the predictions vs the VMAF labels
+print("PCC: " + str(corrcoef(VMAF_labels, predictions)[0,1]))
+
+# Plot the predictions against the VMAF labels
+fig, ax = plt.subplots()
+ax.scatter(VMAF_labels, predictions)
+ax.set_xlabel("VMAF")
+ax.set_ylabel("ERT-LSB")
+plt.show()
